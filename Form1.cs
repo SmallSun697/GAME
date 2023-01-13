95ca556dd63b83910ef8ff68c86180c7b3ed3f14
李忠陽 <@DESKTOP-JHI9PR5>
2023-01-13 21:36:52 +0800

    加入專案檔案。

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GAME
+{
+    public partial class flying : Form
+    {
+        public flying()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //FormBorderStyle = FixedSingle;
+        }
+    }
+}
