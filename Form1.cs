23c655322891c9ace72549dafff390b7001c1c92
忠陽 李 <SmallSun698@gmail.com>
2023-01-14 17:41:50 +0800

    +setting

--- Form1.cs
+++ Form1.cs
@@ -626,7 +626,6 @@ namespace GAME
                             textBox1.Text = "         setting" + "\r\n" + " " + "\r\n" + O4 + s3 + "\r\n" + " " + "\r\n" + O5 + "          <back";
                             break;
                         case 5:
-
                             break;
                     }
                 }
