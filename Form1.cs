975b8fe72b2db36aa6a30421b83faa81956448c7
忠陽 李 <SmallSun698@gmail.com>
2023-01-14 10:15:18 +0800

    ?

--- Form1.cs
+++ Form1.cs
@@ -68,6 +68,74 @@ namespace GAME
         private void timer1_Tick(object sender, EventArgs e)
         {
 
+        private void welcome_Tick(object sender, EventArgs e)
+        {
+            if (iw5 != 0)
+            {
+                if (iw1 != 0)
+                {
+                    s1 = "";
+                    for(int a = iw1; a > 0; a--)
+                    {
+                        s1 += " ";
+                    }
+                    iw1--;
+                    s1 += "         welcome";
+                }
+                if (iw2 != 0)
+                {
+                    s2 = "";
+                    for (int a = iw2; a > 0; a--)
+                    {
+                        s2 += " ";
+                    }
+                    iw2--;
+                    s2 += "          start";
+                }
+                if (iw3 != 0)
+                {
+                    s3 = "";
+                    for (int a = iw3; a > 0; a--)
+                    {
+                        s3 += " ";
+                    }
+                    iw3--;
+                    s3 += "       how to play";
+                }
+                if (iw4 != 0)
+                {
+                    s4 = "";
+                    for (int a = iw4; a > 0; a--)
+                    {
+                        s4 += " ";
+                    }
+                    iw4--;
+                    s4 += "         setting";
+                }
+                if (iw5 != 0)
+                {
+                    s5 = "";
+                    for (int a = iw5; a > 0; a--)
+                    {
+                        s5 += " ";
+                    }
+                    iw5--;
+                    s5 += "          about";
+                }
+                textBox1.Text = O1 + s1 + "\r\n" + O2 + s2 + "\r\n" + O3 + s3 + "\r\n" + O4 + s4 + "\r\n" + O5 + s5;
+            }
+            else
+            {
+                welcome.Enabled = false;
+                O_TF = true;
+                O1 = " ";
+                O2 = ">";
+                O3 = " ";
+                O4 = " ";
+                O5 = " ";
+                O = 2;
+                textBox1.Text = O1 + s1 + "\r\n" + O2 + s2 + "\r\n" + O3 + s3 + "\r\n" + O4 + s4 + "\r\n" + O5 + s5;
+            }
         }
     }
-}
+}//痾  傳出去啊!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
